e34906c17cacfc6e22891205605c19f84bbb231d
MakVM <verunchikmakarova2011@gmail.com>
2022-05-08 16:11:16 +0300

    Initial Commit

---
+++ transport_sol/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  transport_sol
+//
+//  Created by Vera Makarova on 08.05.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
